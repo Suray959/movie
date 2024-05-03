f20632c45597e5466d257a545b81d4208280093c
S U R A Y  M I R Z E Y E V A <mirzayevasuray3@gmail.com>
2024-05-03 10:18:34 +0400

    movie

---
+++ h.js
@@ -0,0 +1,48 @@
+const apiKey = 'e3ce9040';
+const searchBtn = document.querySelector('.submit');
+const deleteBtn = document.querySelector('.delete');
+const moviesList = document.querySelector('.movies-list');
+
+async function movie(url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    moviesList.innerHTML = ''; 
+    data.Search.forEach(movie => {
+      const title = document.createElement('p');
+      title.textContent = movie.Title;
+      moviesList.appendChild(title);
+
+      const img = document.createElement('img');
+      img.src = movie.Poster;
+      img.alt = movie.Title;
+      moviesList.appendChild(img); 
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+function act(value) {
+  const mov = `http://www.omdbapi.com/?apikey=${apiKey}&s=${value}`;
+  movie(mov);
+  console.log(mov);
+}
+
+
+
+searchBtn.addEventListener('click', function() {
+  const input = document.querySelector('.input').value;
+  if(input!==""){
+    act(input);
+  } else{
+    alert('Kino adi yazin!!')
+  }
+ 
+});
+
+
+deleteBtn.addEventListener('click', function() {
+
+  moviesList.innerHTML = '';
+});
